a2db6faffe19baf696e204695bc456f9fe521c14
Puja Sharma <pujaasharmaa.19@gmail.com>
2025-08-17 03:19:12 +0530

    add changes

---
+++ Arrays.js
@@ -0,0 +1,4 @@
+const initialArray= [1,2,3];
+for(i=0;i<initialArray.length;i++){
+    console.log(initialArray[i]);
+}
---
+++ Numbers.js
@@ -0,0 +1,4 @@
+console.log(parseInt(42.111));
+console.log(parseInt("42gsnks"));
+console.log(parseInt('67'));
+console.log(parseInt("hsaj23jsj")); // doesn't conbvert to integer, only converts if it has 42 in starting.
