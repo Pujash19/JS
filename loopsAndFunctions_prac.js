edc44fa187bacceb61c7b8e9d381cd0ab5b08707
Puja Sharma <pujaasharmaa.19@gmail.com>
2025-08-24 03:38:45 +0530

    callbacks learnt

--- loopsAndFunctions_prac.js
+++ loopsAndFunctions_prac.js
@@ -14,3 +14,27 @@ return ans;
 
 console.log("Sum is:",findSum(50)); //two different arguments
 console.log("Sum is: "+ findSum(50)); //logs one single string
+
+//callback function
+
+function square(val){
+     return val*val;
+}
+
+function powOfThree(val){
+     return val*val*val;
+}
+
+function pwoOfFour(val){
+     return val*val*val*val;
+}
+
+function calculatePow(a,fnTocall){
+     console.log(fnTocall);
+     return fnTocall(a);
+}
+console.log(calculatePow(4, square));
+//setTimeout(()=>console.log(calculatePow(4, square)), 2000);
+//anonymous function:  no mae to function.
+
+console.log(calculatePow(3, function (val){return val*val})); //here we just defined functioned without name, coz anysways if we define name here we won't be able to call anywhere
